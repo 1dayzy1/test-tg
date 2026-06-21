fd764339696ba295fe79eb148fbf1f034cb0706a
1dayzy1 <dayzy173@gmail.com>
2026-06-21 21:18:18 +0300

    init2

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const tg = window.Telegram.WebApp;
 
-
+const btn = document.querySelector(".submit-btn");
 const send = () =>{
     tg.sendData(JSON.stringify({
         name:"test"
     }))
 
 }
+btn.addEventListener("click", () =>{
+    send()
+})
